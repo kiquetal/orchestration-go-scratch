31ba85453f80177935e8999e8bd12e6405faeb90
kiquetal <kiquetal@gmail.com>
2024-11-11 20:05:00 -0300

    Adding the server for apis

--- worker/api.go
+++ worker/api.go
@@ -10,3 +10,14 @@ type Api struct {
 	Worker  *Worker
 	Router  *chi.Mux
 }
+
+func (a *Api) initRouter() {
+	a.Router = chi.NewRouter()
+	a.Router.Route("/tasks", func(r chi.Router) {
+		r.Post("/", a.StartTaskHandler)
+		r.Get("/", a.GetTaskHandler)
+		r.Route("/{taslID}", func(r1 chi.Router) {
+			r1.Delete("/", a.StopTaskHandler)
+		})
+	})
+}
