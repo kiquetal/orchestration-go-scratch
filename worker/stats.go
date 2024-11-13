ffdae6aa9204efebc181590529e743cc4011e36e
kiquetal <kiquetal@gmail.com>
2024-11-12 23:29:20 -0300

    Adding disk,memory and cpu stats.go

--- worker/stats.go
+++ worker/stats.go
@@ -8,3 +8,39 @@ type Stats struct {
 	CpuStats  *linux.CPUStat
 	LoadStats *linux.LoadAvg
 }
+
+func (s *Stats) MemTotalKb() uint64 {
+	return s.MemStats.MemTotal
+}
+
+func (s *Stats) MemFreeKb() uint64 {
+	return s.MemStats.MemFree
+}
+
+func (s *Stats) MemAvailableKb() uint64 {
+	return s.MemStats.MemAvailable
+}
+
+func (s *Stats) MemUsedKb() uint64 {
+	return s.MemStats.MemTotal - s.MemStats.MemFree
+}
+
+func (s *Stats) MemUsedPercent() float64 {
+	return float64(s.MemUsedKb()) / float64(s.MemTotalKb()) * 100
+}
+
+func (s *Stats) DiskTotal() uint64 {
+	return s.DiskStats.All
+}
+
+func (s *Stats) DiskUsed() uint64 {
+	return s.DiskStats.Used
+}
+
+func (s *Stats) DiskFree() uint64 {
+	return s.DiskStats.Free
+}
+
+func (s *Stats) DiskUsedPercent() float64 {
+	return float64(s.DiskUsed()) / float64(s.DiskTotal()) * 100
+}
